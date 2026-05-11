818713ab4791b6306c8fa1b4b65547e9670f4aec
Olufemi Emmanuel Oladimeji <oladimejiemmanuel801@gmail.com>
2026-05-11 09:56:03 +0100

    dimeji/dev

--- src/components/Filtericon.vue
+++ src/components/Filtericon.vue
@@ -34,7 +34,7 @@ onMounted(() => {
 })
 
 const categoryOptions = computed(() =>
-  teams.value.map(team => ({
+  teams.value.map((team) => ({
     title: team.name,
     value: team.id
   }))
@@ -42,30 +42,26 @@ const categoryOptions = computed(() =>
 </script>
 
 <template>
-    <div class=" items-center justify-between bg-white rounded-2xl shadow-sm p-4 w-full">
-  
-  <!-- Left: Heading -->
-  <h2 class="text-lg font-semibold text-gray-800 ">
-    Category Filter
-  </h2>
+  <div class="items-center justify-between bg-white rounded-2xl shadow-sm p-4 w-full">
+    <!-- Left: Heading -->
+    <h2 class="text-lg font-semibold text-gray-800">Category Filter</h2>
 
-  <!-- Right: Select -->
-  <div class="mt-7 items-center">
-    <v-select
-  v-model="selectedCategory"
-  :items="categoryOptions"
-  item-title="title"
-  item-value="value"
-  label="Select Team"
-  :loading="loading"
-  variant="outlined"
-  density="compact"
-  hide-details
-  rounded="lg"
-  style="max-width: 200px"
-  bg-color="white"
-/>
+    <!-- Right: Select -->
+    <div class="mt-7 items-center">
+      <v-select
+        v-model="selectedCategory"
+        :items="categoryOptions"
+        item-title="title"
+        item-value="value"
+        label="Select Team"
+        :loading="loading"
+        variant="outlined"
+        density="compact"
+        hide-details
+        rounded="lg"
+        style="max-width: 200px"
+        bg-color="white"
+      />
+    </div>
   </div>
-
-</div>
 </template>
