518f4b7e2ab24720d02739b2adb1c1a39f135b75
olaiya everjoy <olaiyaeverjoy02@gmail.com>
2026-06-17 17:43:37 +0100

    new clone

--- src/views/BrowseProjects.vue
+++ src/views/BrowseProjects.vue
@@ -75,8 +75,10 @@ const fetchProject = async () => {
 
 
 //pushes to a new page inside projects 
+//project is passed into the function openFullProjects: must also be passed 
 const openFullProjects = (project) => {
   console.log(project)
+  //without projects being passed: this wont happen router.push('/projects/2')
   router.push(`/projects/${project.id}`)
 
 }
